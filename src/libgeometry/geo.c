3def4f7aebe468f61e998505d509547f6441c09d
azard285 <pavlenkopasa382@gmail.com>
2023-04-25 22:30:06 +0700

    modified geometry

--- src/libgeometry/geo.c
+++ src/libgeometry/geo.c
@@ -4,37 +4,26 @@
 
 #include <libgeometry/geo.h>
 
-vector findVector(point first, point second)
-{
-    vector res = {(second.x - first.x), (second.y - first.y)};
-    return res;
+vector findVector(point first, point second) {
+  vector res = {(second.x - first.x), (second.y - first.y)};
+  return res;
 }
-double lengthVector(vector vec)
-{
-    return sqrt(vec.a * vec.a + vec.b * vec.b);
-}
+double lengthVector(vector vec) { return sqrt(vec.a * vec.a + vec.b * vec.b); }
 
-float perimeterCircle(point center, double rad)
-{
-    return 2 * M_PI * rad;
-}
-float areaCircle(point center, double rad)
-{
-    return M_PI * rad * rad;
-}
+float perimeterCircle(point center, double rad) { return 2 * M_PI * rad; }
+float areaCircle(point center, double rad) { return M_PI * rad * rad; }
 
-float perimeterTriangle(point a, point b, point c, point d)
-{
-    float first = lengthVector(findVector(a, b));
-    float second = lengthVector(findVector(b, c));
-    float third = lengthVector(findVector(c, d));
-    return first + second + third;
+float perimeterTriangle(point a, point b, point c, point d) {
+  float first = lengthVector(findVector(a, b));
+  float second = lengthVector(findVector(b, c));
+  float third = lengthVector(findVector(c, d));
+  return first + second + third;
 }
 float areaTriangle(point a, point b, point c, point d) // По формуле Герона
 {
-    float p = perimeterTriangle(a, b, c, d) / 2;
-    float ta = lengthVector(findVector(a, b));
-    float tb = lengthVector(findVector(b, c));
-    float tc = lengthVector(findVector(c, d));
-    return sqrt(p * (p - ta) * (p - tb) * (p - tc));
+  float p = perimeterTriangle(a, b, c, d) / 2;
+  float ta = lengthVector(findVector(a, b));
+  float tb = lengthVector(findVector(b, c));
+  float tc = lengthVector(findVector(c, d));
+  return sqrt(p * (p - ta) * (p - tb) * (p - tc));
 }
